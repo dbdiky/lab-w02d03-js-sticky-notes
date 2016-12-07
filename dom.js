99a1f8924bff20f07b2e9509c7c234f41b0b509e
Daniel Dicke <bdiky@hotmail.com>
2016-12-07 14:04:44 -0800

    effot 1

--- dom.js
+++ dom.js
@@ -1,1 +1,14 @@
- // JavaScript goes here
+console.log('Diky');
+
+var newDiv;
+var button = document.querySelector('button');
+
+var addNote = function(event){
+    var note = document.createElement('div');
+    note.classList.add('note');
+    var container = document.querySelector('#container');
+    container.appendChild(note);
+    note.setAttribute("contenteditable", "true")
+  };
+
+  button.addEventListener('click', addNote);
